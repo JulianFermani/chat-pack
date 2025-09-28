aab3a454893c755c55f2a6524f3e9dd08bc943ac
Julián <julianfermani45@gmail.com>
2025-09-28 19:46:01 -0300

    refactor: see-bus con state pattern

---
+++ src/whatsapp/features/see-bus/states/see-bus-origin.state.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+import { State } from '../../../shared/interfaces/state.interface';
+import { SeeBusesData } from '../see-bus.session';
+import { WhatsappService } from 'src/whatsapp/application/whatsapp.service';
+import { UserSession } from 'src/whatsapp/session/user-session.interface';
+import { Message } from 'whatsapp-web.js';
+import { destinationPlacesFetcher } from '../infra/destination-fetcher.service';
+import { backOrDelete } from 'src/whatsapp/shared/utils/back-or-delete-message.util';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class SeeBusOriginState implements State<SeeBusesData> {
+  readonly stepId = 2;
+  constructor(
+    private readonly whatsapp: WhatsappService,
+    private readonly config: ConfigService,
+  ) {}
+  async handle(message: Message, session: UserSession<SeeBusesData>) {
+    const cookie = session.data.cookie;
+    const idBusPlaces = session.data.originPlaces;
+    let placeNum: number;
+    if (session.data.numUserOrigin && session.back === true) {
+      placeNum = Number(session.data.numUserOrigin);
+    } else {
+      placeNum = Number(message.body.trim());
+    }
+
+    const placeKeys = Object.keys(idBusPlaces);
+    const keyOriginPlace = placeKeys[placeNum - 1];
+    const valueOriginPlace = idBusPlaces[keyOriginPlace];
+    if (!valueOriginPlace) {
+      await this.whatsapp.sendMessage(message.from, 'Número inválido.');
+      return session;
+    }
+
+    const apiUrl = this.config.get<string>('SEE_BUS_BASE_URL');
+    const url = `${apiUrl}buscador_cmd.php`;
+
+    const data = {
+      cmd: 'buscar_destino_empresa',
+      idLocalidadGobierno: valueOriginPlace.toString(),
+    };
+
+    const destinationPlacesResponse = await destinationPlacesFetcher(
+      cookie,
+      url,
+      data,
+    );
+
+    if (
+      destinationPlacesResponse.messageText === null ||
+      destinationPlacesResponse.places === null
+    ) {
+      await this.whatsapp.sendMessage(
+        message.from,
+        `😞 No se encontraron destinos`,
+      );
+      return;
+    }
+
+    let messageText = `🚏 Enviá el número a dónde vas: \n${destinationPlacesResponse.messageText}`;
+    messageText = backOrDelete(messageText);
+
+    await this.whatsapp.sendMessage(message.from, messageText);
+
+    session.data.destinationPlaces = destinationPlacesResponse.places;
+    session.data.idOrigin = valueOriginPlace.toString();
+    session.data.numUserOrigin = placeNum;
+    session.step = 3;
+    session.back = false;
+    return session;
+  }
+}
