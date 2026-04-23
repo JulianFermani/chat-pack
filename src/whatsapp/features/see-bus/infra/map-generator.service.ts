ad127aa4a326c98cb7e5520bda9d90475f771354
Julián <julianfermani45@gmail.com>
2026-04-23 18:30:36 -0300

    fix(lint): fortalezco tipado en bootstrap y see-bus

--- src/whatsapp/features/see-bus/infra/map-generator.service.ts
+++ src/whatsapp/features/see-bus/infra/map-generator.service.ts
@@ -1,7 +1,16 @@
 import axios from 'axios';
+
 interface MapResponse {
   buffer: Buffer;
   location: string;
+}
+
+interface GeocodeResult {
+  formatted_address: string;
+}
+
+interface GeocodeResponse {
+  results: GeocodeResult[];
 }
 
 export async function seeBusMapGenerator(
@@ -10,25 +19,23 @@ export async function seeBusMapGenerator(
 ): Promise<MapResponse> {
   const mapsApi = process.env.MAPS_API;
   const mapResponse: MapResponse = {
-    // este tipo?
     buffer: Buffer.from(''),
     location: '',
   };
   const zoom = 13;
   const staticMapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=1280x720&maptype=hybrid&markers=color:red%7C${lat},${lng}&key=${mapsApi}`;
 
-  const response = await axios.get(staticMapUrl, {
+  const response = await axios.get<ArrayBuffer>(staticMapUrl, {
     responseType: 'arraybuffer',
   });
 
-  // TODO: TIPAR FUERTEMENTE ESTÁTICO ESTO
-  const { data } = await axios.get(
+  const { data } = await axios.get<GeocodeResponse>(
     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${mapsApi}`,
   );
 
-  const location = data.results[0].formatted_address;
+  const location = data.results[0]?.formatted_address ?? `${lat}, ${lng}`;
 
-  mapResponse.buffer = Buffer.from(response.data, 'binary');
+  mapResponse.buffer = Buffer.from(response.data);
   mapResponse.location = location;
 
   return mapResponse;
