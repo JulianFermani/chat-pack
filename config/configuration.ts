7b805c648f1eea70d4d7dc396523c1fe422e71e8
Julián <julianfermani45@gmail.com>
2026-04-26 19:52:33 -0300

    chore(config): agrego variables de entorno para ntfy

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,8 +6,8 @@ export const configuration = () => ({
   WHATSAPP_CLIENT_ID: process.env.WHATSAPP_CLIENT_ID,
   WHATSAPP_REMOTE_BACKUP_SYNC_INTERVAL_MS:
     process.env.WHATSAPP_REMOTE_BACKUP_SYNC_INTERVAL_MS,
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+  NTFY_BASE_URL: process.env.NTFY_BASE_URL,
+  NTFY_TOPIC: process.env.NTFY_TOPIC,
   APP_NAME: process.env.APP_NAME,
   MAPS_API: process.env.MAPS_API,
   SEE_BUS_BASE_URL: process.env.SEE_BUS_BASE_URL,
