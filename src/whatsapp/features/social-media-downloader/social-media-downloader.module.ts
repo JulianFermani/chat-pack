cd8b38f22c240b6535f9504a39ded74ba296b581
Julian <julianfermani45@gmail.com>
2026-05-24 15:44:43 +0000

    refactor(social): agrupa descarga social

--- src/whatsapp/features/social-media-downloader/social-media-downloader.module.ts
+++ src/whatsapp/features/social-media-downloader/social-media-downloader.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 
 import { WhatsappModule } from '@client/whatsapp.module';
 import { CommandRegistryModule } from '@command-registry/command-registry.module';
-import { SocialDownloadCommand } from './social-download.command';
-import { SocialDownloadHandler } from './social-download.handler';
+import { SocialDownloadCommand } from './social-download/social-download.command';
+import { SocialDownloadHandler } from './social-download/social-download.handler';
 import { SocialDownloadPolicyService } from './social-download-policy.service';
 import { SocialLinkDetectorService } from './social-link-detector.service';
 import { YtDlpDownloaderService } from './yt-dlp-downloader.service';
